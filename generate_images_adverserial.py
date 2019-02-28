05423faa6150344e51d15ec57c7f675ff065ca8f
Niels G. W. Serup <ngws@metanohi.name>
2019-02-28 16:00:23 +0100

    Generate adverserial for the warp optimisation.
    'data/cuda/no-conflicts-warp-10000000.dat' is designed to work equally well with
    and without the warp optimisation: Even without rearranging which histograms are
    associated with which threads, there should still be no intra-warp conflicts.

--- generate_images_adverserial.py
+++ generate_images_adverserial.py
@@ -2,17 +2,18 @@
 
 import numpy as np
 from os import getcwd
-from os.path import join
+import os.path
 
 # data path
-path = join(getcwd(), "data/cuda")
+path = os.path.join(getcwd(), "data/cuda")
 
 def save(f, fname):
     img_size, img = f()
     img = np.insert(img, 0, img_size)
-    fname = join(path, fname + '-{}'.format(img_size))
-    print 'Saving {}'.format(fname)
-    np.savetxt(fname, img, fmt="%d", delimiter="\n")
+    fname = os.path.join(path, fname + '-{}.dat'.format(img_size))
+    if not os.path.exists(fname):
+        print 'Saving {}'.format(fname)
+        np.savetxt(fname, img, fmt="%d", delimiter="\n")
 
 # generate adverserial images
 
@@ -20,4 +21,10 @@ def zeros():
     img_size = 10000000
     return img_size, np.zeros(img_size)
 
+def no_conflicts_warp():
+    factor = 312500
+    img_size = 32 * factor # 10 million
+    return img_size, np.tile(np.arange(32), factor)
+
 save(zeros, "zeros")
+save(no_conflicts_warp, "no-conflicts-warp")
